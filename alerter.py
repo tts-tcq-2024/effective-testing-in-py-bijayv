ead14701e3b02163f60f54b43be582ae41fbcf7a
bijayv <nrbijay52@gmail.com>
2024-08-10 17:57:22 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -22,4 +22,5 @@ def alert_in_celcius(farenheit):
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
 print(f'{alert_failure_count} alerts failed.')
+assert(alert_failure_count == 2)
 print('All is well (maybe!)')
